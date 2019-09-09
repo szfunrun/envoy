d38785d799e4348b901fc0c540e84ce792e175ea
jiangsy <sanyi0127@sina.com>
2019-09-09 09:26:14 +0800

    Hello, QTSF

---
+++ tools/qtsf/HelloQtsf.cc
@@ -0,0 +1,11 @@
+#include "envoy/buffer/buffer.h"
+#include <iostream>
+using namespace Envoy::Buffer;
+
+int main()
+{
+    RawSlice slice;
+    slice.len_ = 1;
+	std::cout << "Hello, Qtsf!" << std::endl;
+    return 0;
+}
